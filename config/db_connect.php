b04a5ee06e52b46b3221af6f2899e4e53304ce64
IrinaKom7 <irinka.kom89@gmail.com>
2022-01-25 19:04:01 +0300

    added mysql

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,9 +1,26 @@
 <?php
 
-    $host = 'localhost'; 
-	$user = 'root'; //имя пользователя, по умолчанию это root
-	$password = ''; //пароль, по умолчанию пустой
-	$db_name = 'wikidb'; 
+bba1f3171b1234:312f7d06
+
+if ($_SERVER['SERVER_NAME'] == "ira-kom-app.herokuapp.com/") {
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$dbname = substr($url["path"], 1);
+} else {
+	$host = 'localhost';
+	$dbname = 'wikidb';
+	$username = 'root';
+	$password = '';
+}
+
+
+
+    // $host = 'localhost'; 
+	// $user = 'root'; //имя пользователя, по умолчанию это root
+	// $password = ''; //пароль, по умолчанию пустой
+	// $db_name = 'wikidb'; 
 
     //Соединяемся с базой данных используя наши доступы:
 	$link = mysqli_connect($host, $user, $password, $db_name)
