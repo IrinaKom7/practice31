b5c98d00309384da0953e518d862fbfaa370b149
IrinaKom7 <irinka.kom89@gmail.com>
2022-01-28 19:14:35 +0300

    added mysql

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8e9c0785a92cb816ed17207b977017e818685eef',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e9c0785a92cb816ed17207b977017e818685eef',
+    ),
+  ),
+);
